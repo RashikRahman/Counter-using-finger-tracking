904962e5653e8962093ab6a406db94161a5eb0ff
Rashik_Rahman_Pritom <57052944+RashikRahman@users.noreply.github.com>
2021-07-17 07:19:40 +0600

    update

--- Counter.py
+++ Counter.py
@@ -17,30 +17,11 @@ pTime = 0
 tipIds = [4, 8, 12, 16, 20]
 
 while True:
+    totalFingers=0
     success, img = cap.read()
-    img = detector.findHands(img)
-    lmList = detector.findPosition(img, draw=False)
-    if len(lmList) != 0:
-        fingers = []
-
-        # Thumb
-        if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:
-            fingers.append(1)
-        else:
-            fingers.append(0)
-
-        # 4 Fingers
-        for id in range(1, 5):
-            if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
-                fingers.append(1)
-            else:
-                fingers.append(0)
-
-        # print(fingers)
-        totalFingers = fingers.count(1)
-        # print(totalFingers)
-
-        img[0:180,0:130] = OverlayFingerImages[totalFingers-1]
+    totalFingers = detector.fingerUp(img)
+    print(totalFingers)
+    img[0:180,0:130] = OverlayFingerImages[totalFingers-1]
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime=cTime
--- HandTrackModule.py
+++ HandTrackModule.py
@@ -14,6 +14,7 @@ class handDetector():
         self.hands = self.mpHands.Hands(self.mode, self.maxHands,
                                         self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
+        # self.totalFingers=0
 
     def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -42,6 +43,31 @@ class handDetector():
                     cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
 
         return lmList
+    
+    def fingerUp(self,img):
+        totalFingers=0
+        tipIds = [4, 8, 12, 16, 20]
+        img = self.findHands(img)
+        lmList = self.findPosition(img, draw=False)
+        if len(lmList) != 0:
+            fingers = []
+
+            # Thumb
+            if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+            # 4 Fingers
+            for id in range(1, 5):
+                if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+
+            # print(fingers)
+            totalFingers = fingers.count(1)
+        return totalFingers
 
 
 def main():
